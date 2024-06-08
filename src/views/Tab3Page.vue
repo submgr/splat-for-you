234c334dbca1430aa0b244fe05fd8dcd0cd73a70
Aram Virabyan <googlaram200@gmail.com>
2024-06-08 18:39:19 +0300

    upd

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -8,7 +8,13 @@
       </div>
 
       <div>
-        <iframe src="https://www.canva.com/design/DAGHi4PtmTA/ipodtxk87_JoWotqaeTh7A/watch" frameborder="0" style="border:0; width:100%; height:100%;"></iframe>
+        <div style="position: relative; width: 100%; height: 0; padding-top: 177.7778%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGHi4PtmTA&#x2F;ipodtxk87_JoWotqaeTh7A&#x2F;watch?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
       <ion-button expand="block" class="bottom-button" style="margin-bottom: 2vh;" @click="goNextOne">{{currentSliderButtonText}}</ion-button>
       </div>
 
